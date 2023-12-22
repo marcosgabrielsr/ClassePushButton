4e1552406136f02d8c9467219c25d336ddd9ac94
marcosgabrielsr <marcosdevse@gmail.com>
2023-12-22 19:20:04 -0400

    Update PushButton.hpp
    Reorganização das chaves de acesso

--- src/PushButton.hpp
+++ src/PushButton.hpp
@@ -9,16 +9,16 @@
 //Criando Classe PushButton
 class PushButton{
     //Definindo atributos e métodos
+    public:
+        PushButton(const int8_t _pin, const int16_t _time); //Método construtor
+        bool pressButton();                                 //Função que verifica se o botão está pressionado
+        bool clickButton();                                 //Verifica se clicarão o botão
+
     protected:
         unsigned long t;                            //instante do último clique do botão
         const int8_t pin;                           //Pino onde o botão está conectado
         const int16_t interval;                     //Intervalo para controle do efeito Bounce 
         bool oldState;                              //Estado anterior do botão
-    
-    public:
-        PushButton(const int8_t _pin, const int16_t _time); //Método construtor
-        bool pressButton();                                 //Função que verifica se o botão está pressionado
-        bool clickButton();                                 //Verifica se clicarão o botão
 };
 
 #endif
